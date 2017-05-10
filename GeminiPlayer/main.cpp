abf64657f8a1d1d164569d700d4cf4d88ee5d3aa
WIN10 <lmshao@163.com>
2017-05-10 18:44:33 +0800

    v0.1

--- GeminiPlayer/main.cpp
+++ GeminiPlayer/main.cpp
@@ -1,13 +1,3 @@
-#include "SDL.h"
-#include "SDL_thread.h"
-
-extern "C" {
-#include "libavcodec/avcodec.h"
-#include "libavformat/avformat.h"
-#include "libavformat/avio.h"
-#include "libavutil/file.h"
-};
-
 #include "Gemini.h"
 
 int main(int argc, char *argv[])
